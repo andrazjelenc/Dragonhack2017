b0f9d951eb973ed874682c224f6274e27b051f1f
Andraž Jelenc <andraz.jelenc@siol.net>
2017-05-20 15:15:07 +0200

    Update run.php
    Version 2: works with two different tables

--- run.php
+++ run.php
@@ -15,104 +15,46 @@ $PDO = new PDO('mysql:dbname='.$db['db'].';host='.$db['host'],
     $db['user'], $db['pass'], 
     array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 
-$data = array();	//formated database data	
-
-//get all necessary data
-$sql = "
-			SELECT *
-			FROM `zastavice`
-		";
-		
-$query = $PDO->query($sql);
-		
-if ($query->rowCount() > 0)
-{
-	$parsed = $query->fetchAll(PDO::FETCH_ASSOC);
-	foreach($parsed as $predmet)
-	{
-		$id = $predmet['id_predmeta'];
-
-		//build flag array
-		$flags = array();
-		foreach($predmet as $key => $value)
-		{
-			if($key == 'id_predmeta')
-			{
-				continue;
-			}
-			$flags[] = $value;
-		}
-		$data[$id] = $flags;
-	}
-	
-}
-else
-{
-	echo "NAPAKA 1";
-}
-
-//var_dump($data);
-
 /**
- *	COMPARISON
+ *	CLIENT INPUT
  */
 
-$result = array();	//id and factor
+ //todo: beri request
+$userInput = '0.40;1,-1,1,-1;1,1,0';
 
-//get stream from 
-$dataStream = '0,-1,1,1';
-$splittedStream = explode(',', $dataStream);
+$splittedInput = explode(';', $userInput);
 
-//start with comparison
-foreach($data as $predmet_id => $flags)
+$razmerje = $splittedInput[0];
+
+$flags_izvedba = explode(',', $splittedInput[1]);
+$flags_podrocje = explode(',', $splittedInput[2]);
+
+$points_izvedba = get_points('izvedba', $flags_izvedba, $PDO);	//array of points per course
+$points_podrocje = get_points('vsebina', $flags_podrocje, $PDO);
+
+//var_dump($points_izvedba);
+//var_dump($points_podrocje);
+
+$points_skupaj = array();
+
+$predmet_ids = array_keys($points_izvedba);
+
+foreach($predmet_ids as $predmet_id)
 {
-	//for course $predmet_id calculate difference between $splittedStream and $flags
+	$total = $razmerje * $points_izvedba[$predmet_id] + 
+						(1- $razmerje) * $points_podrocje[$predmet_id];
+	$total = round($total,1);
 	
-	if(count($splittedStream) != count($flags))
-	{
-		echo "NAPAKA 2";
-		die;
-	}
-	
-	$factor = 0;
-
-	for($i = 0; $i < count($flags); $i++)
-	{
-		if($splittedStream[$i] == 0)
-		{
-			//we don't care about this flag
-			continue;
-		}
-		else if($splittedStream[$i] == $flags[$i])
-		{
-			//it's ok
-			continue;
-		}
-		else if($splittedStream[$i] == -1 && $flags[$i] == 1)
-		{
-			//i don't want, but the course has it
-			$factor += 5;
-		}
-		else if($splittedStream[$i] == 1 && $flags[$i] == -1)
-		{
-			//i want, but the course doesn't have iterator_apply
-			$factor += 3;
-		}
-		else
-		{
-			echo "NAPAKA 3";
-		}
-	}
-	
-	$result[$predmet_id] = $factor;
+	$points_skupaj[$predmet_id] = $total;
 }
 
+//var_dump($points_skupaj);
 /**
  *	GENERATE OUTPUT
  */
 
 $outputStream = '';
-foreach($result as $predmet_id => $factor)
+foreach($points_skupaj as $predmet_id => $factor)
 {
 	$outputStream .= $factor . ',';
 }
@@ -120,5 +62,94 @@ $outputStream = substr($outputStream, 0, -1);
 
 echo $outputStream;
 
+function get_points($table, $userFlags, $PDO)
+{
+	$data = array();	//formated database data	
 
+	//get all necessary data
+	$sql = "
+				SELECT *
+				FROM `zastavice_".$table."`
+			";
+			
+	$query = $PDO->query($sql);
+			
+	if ($query->rowCount() > 0)
+	{
+		$parsed = $query->fetchAll(PDO::FETCH_ASSOC);
+		foreach($parsed as $predmet)
+		{
+			$id = $predmet['id_predmeta'];
+
+			//build flag array
+			$flags = array();
+			foreach($predmet as $key => $value)
+			{
+				if($key == 'id_predmeta')
+				{
+					continue;
+				}
+				$flags[] = $value;
+			}
+			$data[$id] = $flags;
+		}
+	}
+	else
+	{
+		echo "NAPAKA 1";
+	}
+
+	//var_dump($data);
+
+	/**
+	 *	COMPARISON
+	 */
+
+	$result = array();	//id and factor
+
+	//start with comparison
+	foreach($data as $predmet_id => $flags)
+	{
+		//for course $predmet_id calculate difference between $userFlags and $flags
+		
+		if(count($userFlags) != count($flags))
+		{
+			echo "NAPAKA 2";
+			die;
+		}
+		
+		$factor = 0;
+
+		for($i = 0; $i < count($flags); $i++)
+		{
+			if($userFlags[$i] == 0)
+			{
+				//we don't care about this flag
+				continue;
+			}
+			else if($userFlags[$i] == $flags[$i])
+			{
+				//it's ok
+				continue;
+			}
+			else if($userFlags[$i] == -1 && $flags[$i] == 1)
+			{
+				//i don't want, but the course has it
+				$factor += 5;
+			}
+			else if($userFlags[$i] == 1 && $flags[$i] == -1)
+			{
+				//i want, but the course doesn't have iterator_apply
+				$factor += 3;
+			}
+			else
+			{
+				echo "NAPAKA 3";
+			}
+		}
+		
+		$result[$predmet_id] = $factor;
+	}
+	return $result;
+}
 ?>
